265632f7ca7095a3c80866ccf88fd06f19fb5117
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-17 13:24:03 +0200

    disablehwc.sh: make it possible to disable the hwc overlay's via system prop.

--- configs/disablehwc.sh
+++ configs/disablehwc.sh
@@ -1,15 +1,19 @@
 #!/system/bin/sh
 
-(while :
-do
-    sf=$(service list | grep -c "SurfaceFlinger")
+status=$(getprop debug.sf.disable_hwcomposer)
 
-    if [ $sf -eq 1 ]
-    then
-        service call SurfaceFlinger 1008 i32 1
-        break
-    else
-        sleep 2
-    fi
-done
-) &
+if [ "$status" == "true" ] ; then
+  (while :
+  do
+      sf=$(service list | grep -c "SurfaceFlinger")
+
+      if [ $sf -eq 1 ]
+      then
+          service call SurfaceFlinger 1008 i32 1
+          break
+      else
+          sleep 2
+      fi
+  done
+  ) &
+fi
